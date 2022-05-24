f20e894939b07503e530947761ef6b47f6589f4b
marumuji <marumuji@gmail.com>
2022-05-24 23:23:57 +0900

    Add inheritance sample

--- Class.js
+++ Class.js
@@ -1,3 +1,18 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Point = /** @class */ (function () {
     function Point(x, y) {
         this.x = x;
@@ -8,7 +23,24 @@ var Point = /** @class */ (function () {
     };
     return Point;
 }());
+var Point3D = /** @class */ (function (_super) {
+    __extends(Point3D, _super);
+    function Point3D(x, y, z) {
+        var _this = _super.call(this, x, y) || this;
+        _this.z = z;
+        return _this;
+    }
+    Point3D.prototype.add = function (point) {
+        var point2D = _super.prototype.add.call(this, point);
+        return new Point3D(point2D.x, point2D.y, this.z + point.z);
+    };
+    return Point3D;
+}(Point));
 var p1 = new Point(0, 10);
 var p2 = new Point(10, 20);
 var p3 = p1.add(p2);
 console.log(p3);
+var p4 = new Point3D(10, 20, 30);
+var p5 = new Point3D(40, 50, 60);
+var p6 = p4.add(p5);
+console.log(p6);
--- Class.ts
+++ Class.ts
@@ -10,7 +10,25 @@ class Point {
   }
 }
 
+class Point3D extends Point {
+  z: number;
+  constructor(x: number, y:number, z:number){
+    super(x, y);
+    this.z = z;
+  }
+  add(point: Point3D) {
+    var point2D = super.add(point);
+    return new Point3D(point2D.x, point2D.y, this.z + point.z);
+  }
+}
+
 var p1 = new Point(0, 10);
 var p2 = new Point(10, 20);
 var p3 = p1.add(p2);
-console.log(p3)
+console.log(p3);
+
+var p4 = new Point3D(10, 20, 30);
+var p5 = new Point3D(40, 50, 60);
+var p6 = p4.add(p5);
+console.log(p6);
+
