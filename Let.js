30b2e03313a3c655af8513b8b5ecf490601122da
marumuji <marumuji@gmail.com>
2022-05-27 00:00:43 +0900

    Modified using local variable

--- Let.js
+++ Let.js
@@ -1,9 +1,10 @@
 var funcs = [];
 for (var i = 0; i < 3; i++) {
+    console.log("i=" + i);
     funcs.push(function () {
         console.log(i);
     });
 }
 for (var j = 0; j < 3; j++) {
-    funcs[j]();
+    // funcs[j]();
 }
--- Let.ts
+++ Let.ts
@@ -1,8 +1,11 @@
 var funcs = [];
 for (var i = 0; i < 3; i++) {
-  funcs.push(function() {
-    console.log(i);
-  })
+  (function() {
+    var local = i;
+    funcs.push(function() {
+      console.log(local)
+    })
+  })();
 }
 
 for (var j = 0; j < 3; j++) {
