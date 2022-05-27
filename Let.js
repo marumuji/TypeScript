b8cb7712327b1e8ca57b830d0be69e2bdee72029
marumuji <marumuji@gmail.com>
2022-05-27 23:05:42 +0900

    Change for loop var to let

--- Let.js
+++ Let.js
@@ -1,10 +1,12 @@
 var funcs = [];
-for (var i = 0; i < 3; i++) {
-    console.log("i=" + i);
+var _loop_1 = function (i) {
     funcs.push(function () {
         console.log(i);
     });
+};
+for (var i = 0; i < 3; i++) {
+    _loop_1(i);
 }
 for (var j = 0; j < 3; j++) {
-    // funcs[j]();
+    funcs[j]();
 }
--- Let.ts
+++ Let.ts
@@ -1,11 +1,8 @@
 var funcs = [];
-for (var i = 0; i < 3; i++) {
-  (function() {
-    var local = i;
-    funcs.push(function() {
-      console.log(local)
-    })
-  })();
+for (let i = 0; i < 3; i++) {
+  funcs.push(function() {
+    console.log(i)
+  })
 }
 
 for (var j = 0; j < 3; j++) {
