91d57e21e7a0b3869ad1de1bb6947f83819bcb7c
marumuji <marumuji@gmail.com>
2022-05-29 23:45:50 +0900

    Change apply to spread operator

---
+++ ForOf.ts
@@ -0,0 +1,4 @@
+var someArray = [9, 2, 5];
+for (var item in someArray) {
+  console.log(item);
+}
--- SpreadOperator.ts
+++ SpreadOperator.ts
@@ -1,6 +1,6 @@
-function hoge(x, y, z) { 
+function foo(x, y, z) { 
   console.log(x, y, z);
 }
 var args = [0, 1, 2];
 // foo.apply(null, args)
-hoge(...args);
+foo(...args);
