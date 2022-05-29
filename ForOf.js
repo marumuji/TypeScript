b217c5f68e5af60580a91cc560e600b93a1b36e9
marumuji <marumuji@gmail.com>
2022-05-29 23:48:35 +0900

    Add for sample

--- ForOf.js
+++ ForOf.js
@@ -2,3 +2,7 @@ var someArray = [9, 2, 5];
 for (var item in someArray) {
     console.log(item);
 }
+for (var _i = 0, someArray_1 = someArray; _i < someArray_1.length; _i++) {
+    var item2 = someArray_1[_i];
+    console.log(item2);
+}
--- ForOf.ts
+++ ForOf.ts
@@ -2,3 +2,7 @@ var someArray = [9, 2, 5];
 for (var item in someArray) {
   console.log(item);
 }
+
+for (var item2 of someArray) {
+  console.log(item2);
+}
