548924e3864326aa2cc84a28b07334edf3b839fe
marumuji <marumuji@gmail.com>
2022-05-31 00:20:36 +0900

    Add Generator sample

---
+++ Generator.ts
@@ -0,0 +1,12 @@
+function* idMaker() {
+  let index = 0;
+  while(index < 3) {
+    yield index++;
+  }
+}
+
+let gen = idMaker();
+console.log(gen.next());
+console.log(gen.next());
+console.log(gen.next());
+console.log(gen.next());
