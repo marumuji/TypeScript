f6f69f10f6d9f58fe23b37c53c53c9954b5dc8d6
marumuji <marumuji@gmail.com>
2022-06-06 22:34:46 +0900

    Add Generics Sample

---
+++ Generics.ts
@@ -0,0 +1,18 @@
+function reverse<T>(items: T[]): T[] {
+  var toreturn = [];
+  for (let i = items.length - 1; i >= 0; i--) {
+    toreturn.push(items[i]);
+  }
+  return toreturn;
+}
+
+var sample = [1, 2, 3];
+var reversed = reverse(sample);
+console.log(reversed);
+
+// reversed[0] = '1'; 
+// reversed = ['1', '2'];
+
+reversed[0] = 1;
+reversed = [1, 2];
+console.log(reversed);
