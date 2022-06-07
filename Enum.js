44ecca1a49d3037aedff76e26f34537f5e5ef7e2
marumuji <marumuji@gmail.com>
2022-06-07 22:59:51 +0900

    Add Enum sample

---
+++ Enum.js
@@ -0,0 +1,11 @@
+var CardSuit;
+(function (CardSuit) {
+    CardSuit[CardSuit["Clubs"] = 0] = "Clubs";
+    CardSuit[CardSuit["Diamonds"] = 1] = "Diamonds";
+    CardSuit[CardSuit["Hearts"] = 2] = "Hearts";
+    CardSuit[CardSuit["Spades"] = 3] = "Spades";
+})(CardSuit || (CardSuit = {}));
+var card = CardSuit.Clubs;
+console.log(card);
+card = CardSuit.Diamonds;
+card = "not a member of card suit";
---
+++ Enum.ts
@@ -0,0 +1,12 @@
+enum CardSuit {
+  Clubs,
+  Diamonds,
+  Hearts,
+  Spades
+}
+
+var card = CardSuit.Clubs;
+
+console.log(card);
+
+card = "not a member of card suit";
