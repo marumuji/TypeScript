4f2bcf59240cc222c29ed028bbf84408af21f54e
marumuji <marumuji@gmail.com>
2022-06-08 23:08:57 +0900

    Add Freshness sample

---
+++ Freshness.js
@@ -0,0 +1,9 @@
+function logName(something) {
+    console.log(something.name);
+}
+var person = { name: 'matt', job: 'being awesome' };
+var animal = { name: 'cow', diet: 'vegan, but has milk of own species' };
+var random = { note: "I don't have a name property" };
+logName(person);
+logName(animal);
+logName(random);
